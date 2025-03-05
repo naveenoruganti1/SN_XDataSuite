561df86e2b42cbae84d750f6f278c03130d57671
sairam566 <sairam1431995@gmail.com>
2025-03-05 07:15:50 +0530

    initial commit

---
+++ src/main/java/com/datasuite/xml/XmlDataSuiteApplication.java
@@ -0,0 +1,13 @@
+package com.datasuite.xml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlDataSuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlDataSuiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datasuite/xml/XmlDataSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datasuite.xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlDataSuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
